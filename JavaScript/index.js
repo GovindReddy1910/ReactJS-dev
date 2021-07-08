1e0442c32246575b69138a45fb9a7378bcdd8cf7
Govind Reddy <kundhurugovindreddy@gmail.com>
2021-07-08 10:00:00 +0530

    "html-cc-first-commit"

---
+++ JavaScript/index.js
@@ -0,0 +1,87 @@
+        // var sellingPrice = 3490;
+        // var listingPrice = 1299;
+        // var discontPrice = ((sellingPrice-listingPrice)/sellingPrice)*100;
+        // console.log(Math.round(discontPrice));
+        
+// var a =12;
+// var b=20;
+// var result =a>b;
+// console.log(result);
+// if(a>b) {
+//         console.log(a)
+// }
+// else if (b>a){
+//         console.log('i am printing'+a);
+//         console.log(b);
+// }
+// else{
+// console.log('No data');
+
+// }        
+// -------------------
+// var age =70;
+
+// if(age<=3){
+//         console.log('infact'); 
+// }
+// else if(age>3 && age<=12){
+//         console.log('child');
+// }
+// else if(age>12 && age<=19){
+//         console.log('teen')
+// }
+// else if(age>19 && age<=60){
+//         console.log('adult');
+// }
+// else{
+//         console.log("old age");
+// }
+// ---------------
+// var marks = 490;
+// if (marks<=250){
+//         console.log('fail')
+// }
+// else if(marks>250 && marks<=300) {
+//         console.log('avg')
+// } 
+// else if(marks>300 && marks<=450){
+//         console.log('Good')
+// }
+// else if(marks>450 && marks<=500){
+//         console.log('vary Good')
+// }
+// else {
+//         console.log('TOP 10')
+// }
+
+var whichPerson = "admin";
+       switch(whichPerson){
+               case "admin":
+               console.log("you have authority in a website") 
+       } break;
+       switch(whichPerson){
+        case "sub-admin":
+        console.log("access  for after admin level") 
+        } break;
+        switch(whichPerson){
+                case "Manager":
+                console.log("not eligible for rides") 
+        } break;
+        switch(whichPerson){
+                case "executive":
+                console.log("Below of admin level") 
+        } break;
+
+
+        switch (key) {
+                case 'Admin':
+                        console.log("admin")
+                        break;
+                case "Sub-Admin":
+                        console.log('Sub-admin');
+                        break;        
+        
+                default:
+                        break;
+        }
+
